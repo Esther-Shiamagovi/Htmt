a921680c2db753b711e15822730b1be7e9d7b2bf
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-03-07 21:52:52 +0300

    added a file

---
+++ index.js
@@ -0,0 +1,34 @@
+let b = "extravaganza"
+console.log('item',b[8]);
+console.log('item',b[9]);
+console.log('item',b[10]);
+console.log('item',b[11]);
+
+const food ="The quick fox jumped over the lazy dog";
+console.log(food.slice(0,4) + "eat" + food.slice(4) );
+
+const story = "The quick brown fox jumped over the lazy dog";
+console.log(story.match(/the/g).length);
+console.log(story.match(/brown/g).length);
+
+const string1="The pupils are reading in the library";
+const string2="The child was sitting on the table before it fell";
+
+let result1=string1.search("are");
+let result2=string2.search("sitting");
+
+console.log(result1);
+console.log(result2);
+
+
+let a = "wonderful";
+console.log(b.toUpperCase());
+let d = "amazing";
+console.log(d.toLowerCase());
+let c = "undERneath";
+console.log(c.toLowerCase());
+let f = "A wonderfull world";
+console.log(f.replace("w",'W') );
+
+
+
