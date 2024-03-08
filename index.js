8cd0c50c7359852b8f5966ded26838a4272e80e4
Esther-Shiamagovi <shiamagoviesther@gmail.com>
2024-03-08 16:36:03 +0300

    modified file

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+//extraction of character from a string
 let b = "extravaganza"
 console.log('item',b[8]);
 console.log('item',b[9]);
 console.log('item',b[10]);
 console.log('item',b[11]);
-
+//Sliced to add eat ady added the whole string to the remaining part
 const food ="The quick fox jumped over the lazy dog";
 console.log(food.slice(0,4) + "eat" + food.slice(4) );
 
+//counting the number of times the string appears in a string by matching is
 const story = "The quick brown fox jumped over the lazy dog";
 console.log(story.match(/the/g).length);
 console.log(story.match(/brown/g).length);
-
+//finding a word in a string
 const string1="The pupils are reading in the library";
 const string2="The child was sitting on the table before it fell";
 
@@ -20,7 +22,7 @@ let result2=string2.search("sitting");
 console.log(result1);
 console.log(result2);
 
-
+//Turning strings to different forms using in built functions
 let a = "wonderful";
 console.log(b.toUpperCase());
 let d = "amazing";
